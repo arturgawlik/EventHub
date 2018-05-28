bc126530350e2b3d9f9482a9e09542d9c6c1f859
KamilCzernecki <czernecki.kamil@me.com>
2018-05-28 09:46:02 +0200

    footer changes

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthService} from '../services/auth.service';
 import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
 
 @Component({
@@ -8,16 +6,11 @@ import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent{
 
-  constructor(private router: Router, public authService: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  logout() {
-    this.authService.logout();
-    this.router.navigate(['Login']);
   }
 
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['Login']);
   }
 
 }
