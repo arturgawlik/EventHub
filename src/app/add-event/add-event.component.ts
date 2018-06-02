69a662f8f0dd7e7609c10d2fdf67f1d7f970a9b0
KamilCzernecki <czernecki.kamil@me.com>
2018-06-02 12:19:14 +0200

    validation

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-event',
   templateUrl: './add-event.component.html',
   styleUrls: ['./add-event.component.css']
 })
-export class AddEventComponent {
+export class AddEventComponent implements OnInit {
+
+  addEventForm: FormGroup;
 
   constructor() { }
 
+  ngOnInit(): void {
+    this.createForm();
+  }
 
   onSubmit() {
-    console.log('submit!');
+    console.log(this.addEventForm.value);
+  }
+
+  private createForm(){
+    this.addEventForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      description: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      startDate: new FormControl('', [Validators.required]),
+      endDate: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    })
   }
 
 }
